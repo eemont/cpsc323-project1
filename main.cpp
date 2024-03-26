57098ff2efdeb5f73ff7c2492e997174fab821d7
Emmanuel Montoya <emmanuelmontoya02@gmail.com>
2024-03-26 03:04:10 +0000

    Created a function that reads and prints out file's code onto terminal. Still trying to figure out how to approach project.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-//remove comments function
+// The code below simply just reads the file and outputs it into the terminal
+// The code we'll be testing is input.cpp
 
-// tokenize a line function
-
-// main function
 int main() {
+    string filename;
 
+    // Open the file
+    ifstream file("input.cpp");
 
+    // Check if the file is opened successfully
+    if (!file.is_open()) {
+        cout << "Error opening file: " << filename << endl;
+        return 1; // Return error code
+    }
+
+    // Read and output the contents of the file
+    string line;
   
+    while (getline(file, line)) {
+        cout << line << endl;
+    }
+
+    // Close the file
+    file.close();
+
+    return 0; // Return success
 }
